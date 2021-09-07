3bd9d3f9fef4a592718fbdedccb36bbc4755f393
radehrgo96@gmail.com <radehrgo96@gmail.com>
2021-09-07 20:28:14 +0200

    Exercises from 61 to 62

---
+++ exercise.50/Exercise.61/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise._61
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first string:");
+            string s1 = Console.ReadLine();
+
+            Console.WriteLine("Enter the second string:");
+            string s2 = Console.ReadLine();
+
+            Console.WriteLine(s1.Insert(s1.Length / 2, s2));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ exercise.50/Exercise.62/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise._62
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string s1 = Console.ReadLine();
+
+            string newString = s1.Substring(s1.Length - 2);
+
+            Console.WriteLine(newString + newString + newString);
+            Console.ReadLine();
+        }
+    }
+}
